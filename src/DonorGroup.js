19fa8d473c425581e6b76c6f5b6859e4abfc07b9
rachelleahklein <Mac@Mavericks-79.local>
2017-08-03 20:42:22 -0700

    Add ability to pull data with fetch

--- src/DonorGroup.js
+++ src/DonorGroup.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import DonorList from './DonorList'
-import donors from './donors.json';
 
 class DonorGroup extends React.Component {
   constructor(props) {
   super(props);
   this.state = {
-    donors: donors
+    donors: [],
+    url: props.url
   }
+  this.hitServer = this.hitServer.bind(this);
 }
-// componentDidMount() {
-//   this.setState({donors: newDonors});
-// }
+
+hitServer(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => this.setState({ donors: data }))
+    .catch(error => console.log(error))
+}
+
+componentDidMount() {
+  this.hitServer('http://54.213.83.132/hackoregon/http/oregon_business_contributors/5/');
+}
+
   render() {
     return (
       <DonorList donors={this.state.donors} />
