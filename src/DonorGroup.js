6b92f7bffc12a41558d529fa4e4fa5dfb5f384a8
rachelleahklein <Mac@Mavericks-79.local>
2017-08-05 18:12:22 -0700

    Add ability to display both individual and business donors

--- src/DonorGroup.js
+++ src/DonorGroup.js
@@ -1,30 +1,45 @@
 import React from 'react';
 import DonorList from './DonorList'
 
+const BUSINESS_API = 'http://54.213.83.132/hackoregon/http/oregon_business_contributors/5/';
+const DONOR_API = 'http://54.213.83.132/hackoregon/http/oregon_individual_contributors/5/'
+
 class DonorGroup extends React.Component {
   constructor(props) {
   super(props);
   this.state = {
-    donors: [],
-    url: props.url
+    businesses: [],
+    donors: []
   }
-  this.hitServer = this.hitServer.bind(this);
+  this.hitBusinessServer = this.hitBusinessServer.bind(this);
+  this.hitDonorServer = this.hitDonorServer.bind(this);
 }
 
-hitServer(url) {
-  return fetch(url)
+hitBusinessServer() {
+  return fetch(BUSINESS_API)
+    .then(response => response.json())
+    .then(data => this.setState({ businesses: data }))
+    .catch(error => console.log(error))
+}
+
+hitDonorServer() {
+  return fetch(DONOR_API)
     .then(response => response.json())
     .then(data => this.setState({ donors: data }))
     .catch(error => console.log(error))
 }
 
 componentDidMount() {
-  this.hitServer('http://54.213.83.132/hackoregon/http/oregon_business_contributors/5/');
+  this.hitBusinessServer();
+  this.hitDonorServer();
 }
 
   render() {
     return (
-      <DonorList donors={this.state.donors} />
+      <div>
+        <DonorList donors={this.state.businesses} />
+        <DonorList donors={this.state.donors} />
+      </div>
     );
   }
 }
