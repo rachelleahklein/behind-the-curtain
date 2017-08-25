ab81014f199b765e63e8db844ba1bed5a8f33156
rachelleahklein <Mac@Mavericks-79.local>
2017-08-24 19:57:20 -0700

    Start styling donor lists/donors

--- src/DonorItem.js
+++ src/DonorItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function DonorItem(props) {
-  return <li>{props.donor.contributor_payee}: {props.donor.sum}</li>;
+  return <li>{props.donor.contributor_payee}: ${(props.donor.sum).toFixed(2)}</li>;
 }
 
 export default DonorItem;
--- src/DonorList.js
+++ src/DonorList.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import DonorItem from './DonorItem'
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  float: left;
+  padding: 1rem;
+  background: lightblue;
+  border: 5px solid #369;
+  margin: 1%;
+  border-radius: 5px;
+`;
+
+const StylishList = styled.ul`
+  list-style: none;
+`;
 
 function DonorList(props) {
   return (
-    <ul>
-      {props.donors.map((donor) =>
-        <DonorItem key={donor.contributor_payee.toString()} donor={donor} />
-      )}
-    </ul>
+    <Wrapper>
+      <StylishList>
+        {props.donors.map((donor) =>
+          <DonorItem key={donor.contributor_payee.toString()} donor={donor} />
+        )}
+      </StylishList>
+    </Wrapper>
   );
 }
 
