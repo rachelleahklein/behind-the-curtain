68744a22a8d0219806ec531b7f6846b179606644
rachelleahklein <Mac@Mavericks-79.local>
2017-08-29 20:56:04 -0700

    Add (a tiny bit of) styling to Summary component

--- src/FormattedSummary.js
+++ src/FormattedSummary.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SummaryWrapper = styled.section`
+  float: left;
+  padding: 2%;
+  background: skyblue;
+  margin: 1% 2.5% 2.5%;
+  border-radius: 5px;
+  width: 50%;
+  font-size: 1.75rem;
+`;
 
 function FormattedSummary(props) {
   const summary = props.summary;
   return (
-    <div>
+    <SummaryWrapper>
       <p>In: ${summary["in"]}</p>
       <p>Out: {summary["out"]}</p>
       <p>From Oregon: {summary["from_within"]}</p>
@@ -12,7 +23,7 @@ function FormattedSummary(props) {
       <p>To outside Oregon: {summary["to_outside"]}</p>
       <p>Total grassroots donations: {summary["total_grassroots"]}</p>
       <p>Total from in state: {summary["total_from_in_state"]}</p>
-    </div>
+    </SummaryWrapper>
   )
 }
 
